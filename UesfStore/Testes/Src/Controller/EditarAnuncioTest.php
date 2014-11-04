726f6d06f7ae22c3cb92e1449a65b15fc3a20ae4
marcelobiao <marcelobiao2@gmail.com>
2014-11-04 17:19:55 -0300

    Update Testes

--- UesfStore/Testes/Src/Controller/EditarAnuncioTest.php
+++ UesfStore/Testes/Src/Controller/EditarAnuncioTest.php
@@ -12,47 +12,97 @@ require_once dirname(__FILE__).'/../../../Src/Controller/Exception/CampoPreenchi
 class EditarAnuncioTest extends PHPUnit_Framework_TestCase {
 
     protected $controller;
-    protected $produto;
-    protected $servico;
-    protected $id;
+    protected $anuncio;
+    protected $idProduto;
 
     /**
      * Método setUp executado antes de todos os testes.
      */
     protected function setUp() {
         $this->controller = new Controller();
-        $this->produto = $this->controller->cadastrarProduto("TV de Plasma", "Eletrônicos",
-                "Televisor semi-novo, 2 meses de uso", "1.100,00", "HHHHSSSS", "1");
-        $this->id = "1";
-
+        $this->controller->cadastrarUsuario("João Filho", "joao@hotmail.com",
+                "M", "07599992222","07599992222","abc1234","abc1234","fotoaqui","0");
+        $this->controller->cadastrarProduto("TV de Plasma", "Eletrônicos", "1.100,00", "HHHHHHH","Televisor semi-novo, 2 meses de uso", "1","2");
+        $this->controller->cadastrarServico("Troca de lâmpadas", "Geral", "1.000.000,99", "HHHHHHH","Serviço rapido, facil e \"barato\" ", "1","2");
+    
     }
 
     /**
      * SC1 - Edição de anuncio de produto bem-sucedida:
      * Testa a edição de um anuncio de produto no caso ideal.
      */
-    public function testEditarAnuncioBemSucedido()
+    public function testEditarProdutoBemSucedido()
     {
+        $this->anuncio = $this->controller->recuperarAnuncio("1");
+        if($this->anuncio instanceof Anuncio)
+        {   
+            $titulo = $this->anuncio->getTitulo();
+            $categoria = $this->anuncio->getCategoria();
+            $preco = $this->anuncio->getPreco();
+            $fotoVideo = $this->anuncio->getFotoVideo();
+            $descricao = $this->anuncio->getDescricao();
+            $idUsuario = $this->anuncio->getIdUsuario();//TODO:PQ ñ reconhece esse metodo?
+            $quantidade = $this->anuncio->getQuantidade();
+        }
+        $this->controller->editarProduto("TV de LED FullHD", "Televisores", "2.000,00", "SSSSSSS","Televisor Novo, Com Nota Fiscal","1","3");
+        $this->anuncio = $this->controller->recuperarAnuncio("1");
+        if($this->anuncio instanceof Anuncio)
+        {
+            $this->assertNotEquals($titulo,$this->anuncio->getTitulo());
+            $this->assertNotEquals($categoria,$this->anuncio->getCategoria());
+            $this->assertNotEquals($preco,$this->anuncio->getPreco());
+            $this->assertNotEquals($fotoVideo,$this->anuncio->getFotoVideo());
+            $this->assertNotEquals($descricao,$this->anuncio->getDescricao());
+            $this->assertEquals($idUsuario,$this->anuncio->getIdUsuario());
+            $this->assertNotEquals($quantidade,$this->anuncio->getQuantidade());
 
-        $anuncio = $this->controller->recuperarAnuncio($this->id);
-        $titulo = $this->anuncio->getTitulo();
-        $categoria = $this->anuncio->getCategoria();
-        $descricao = $this->anuncio->getDescricao();
-        $preco = $this->anuncio->getPreco();
+            $this->assertEquals("TV de LED FullHD", $this->produto->getTitulo());
+            $this->assertEquals("Televisores", $this->produto->getCategoria());
+            $this->assertEquals("2.000,00", $this->produto->getPreco());
+            $this->assertEquals("SSSSSSS", $this->produto->getFotoVideo());
+            $this->assertEquals("Televisor Novo, Com Nota Fiscal", $this->produto->getDescricao());
+            $this->assertEquals("1", $this->produto->getIdUsuario());
+            $this->assertEquals("3", $this->produto->getQuantidade());
+        }
+    }
+    
+    /**
+     * SC2 - Edição de anuncio de serviço bem-sucedida:
+     * Testa a edição de um anuncio de serviço no caso ideal.
+     */
+    public function testEditarServicoBemSucedido()
+    {
+        $this->anuncio = $this->controller->recuperarAnuncio("1");
+        if($this->anuncio instanceof Anuncio)
+        {   
+            $titulo = $this->anuncio->getTitulo();
+            $categoria = $this->anuncio->getCategoria();
+            $preco = $this->anuncio->getPreco();
+            $fotoVideo = $this->anuncio->getFotoVideo();
+            $descricao = $this->anuncio->getDescricao();
+            $idUsuario = $this->anuncio->getIdUsuario();//TODO:PQ ñ reconhece esse metodo?
+            $quantidade = $this->anuncio->getQuantidade();
+        }
+        $this->controller->editarProduto("TV de LED FullHD", "Televisores", "2.000,00", "SSSSSSS","Televisor Novo, Com Nota Fiscal","1","3");
+        $this->anuncio = $this->controller->recuperarAnuncio("1");
+        if($this->anuncio instanceof Anuncio)
+        {
+            $this->assertNotEquals($titulo,$this->anuncio->getTitulo());
+            $this->assertNotEquals($categoria,$this->anuncio->getCategoria());
+            $this->assertNotEquals($preco,$this->anuncio->getPreco());
+            $this->assertNotEquals($fotoVideo,$this->anuncio->getFotoVideo());
+            $this->assertNotEquals($descricao,$this->anuncio->getDescricao());
+            $this->assertEquals($idUsuario,$this->anuncio->getIdUsuario());
+            $this->assertNotEquals($quantidade,$this->anuncio->getQuantidade());
 
-        $this->controller->EditarAnuncio($this->$id, "TV de LED FullHD", "Televisores",
-                "Televisor Novo, Com Nota Fiscal", "2.000,00", "HHHHSSSS","1");
-        $anuncioAtual = $this->controller->recuperarAnuncio($this->$id);
-        $this->assertNotEquals($titulo,$anuncioAtual->getTitulo());
-        $this->assertNotEquals($categoria,$anuncioAtual->getCategoria());
-        $this->assertNotEquals($descricao,$anuncioAtual->getDescricao());
-        $this->assertNotEquals($preco,$anuncioAtual->getPreco());
-
-        $this->assertEquals("TV de LED FullHD", $this->produto->getTitulo());
-        $this->assertEquals("Televisores", $this->produto->getCategoria());
-        $this->assertEquals("Televisor Novo, Com Nota Fiscal", $this->produto->getDescricao());
-        $this->assertEquals("2.000,00", $this->produto->getPreco());
-
+            $this->assertEquals("TV de LED FullHD", $this->produto->getTitulo());
+            $this->assertEquals("Televisores", $this->produto->getCategoria());
+            $this->assertEquals("2.000,00", $this->produto->getPreco());
+            $this->assertEquals("SSSSSSS", $this->produto->getFotoVideo());
+            $this->assertEquals("Televisor Novo, Com Nota Fiscal", $this->produto->getDescricao());
+            $this->assertEquals("1", $this->produto->getIdUsuario());
+            $this->assertEquals("3", $this->produto->getQuantidade());
+        }
     }
 
      /**
@@ -63,9 +113,8 @@ class EditarAnuncioTest extends PHPUnit_Framework_TestCase {
     public function testEditarAnuncioSemTitulo()
     {
 
-        $this->controller->EditarAnuncio($this->produto, "", "Eletrônicos",
-                "Televisor semi-novo, 2 meses de uso", "1.100,00", "HHHHSSSS", "1");
-
+        $this->controller->editarProduto("", "Televisores", "2.000,00", "SSSSSSS","Televisor Novo, Com Nota Fiscal","1","3");
+        
     }
 
     /**
@@ -75,9 +124,19 @@ class EditarAnuncioTest extends PHPUnit_Framework_TestCase {
      */
     public function testEditarAnuncioComPrecoInvalido()
     {
-        $this->controller->EditarAnuncio($this->produto, "TV de Plasma", "Eletrônicos",
-                "Televisor semi-novo, 2 meses de uso", "", "HHHHSSSS", "1");
-
+        $this->controller->editarProduto("TV de LED FullHD", "Televisores", "", "SSSSSSS","Televisor Novo, Com Nota Fiscal","1","3");
+        
+    }
+    
+    /**
+     * SC4 - Edição de anuncio com descrição invalida:
+     * Testa a edição de um anúncio sem a descrição.
+     *@expectedException CampoPreenchidoErradoException
+     */
+    public function testEditarAnuncioComDescricaoInvalida()
+    {
+        $this->controller->editarProduto("TV de LED FullHD", "Televisores", "", "SSSSSSS","","1","3");
+        
     }
 
 }
