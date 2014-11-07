beb46515186ed44be16af8c6a3ee6dbe058d2229
arthurferroni <arthur.ferroni@live.com>
2014-11-07 09:00:31 -0300

    Admim marcelo

--- UesfStore/Src/DAO/UsuarioDAO.php
+++ UesfStore/Src/DAO/UsuarioDAO.php
@@ -2,52 +2,41 @@
 require_once('MySqlClass.php');
 conexao();
 
-function _inserir($Email,$Nome,$Senha,$Telefone1,$Telefone2,$Data_Nascimento,$Foto,$Sexo )
+function inserir($nome,$email,$sexo,$telefone,$celular,$senha,$foto,$status)
 {
 
-    $sql_inserir = "INSERT INTO  `u961758316_uefs`.`usuarios` VALUES (NULL ,  '$Email',  '$Nome',  "
-        . "'$Senha',  '$Telefone1',  '$Data_Nascimento',  '$Sexo',  '$Telefone2',  '$Foto',  '0',  '0',  '0',  '0');";
-
+    $sql_inserir = "INSERT INTO  `u961758316_uefs`.`usuarios` VALUES (NULL ,  '$email',  '$nome',  "
+        . "'$senha',  '$telefone', NULL,  '$Sexo',  '$celular',  '$foto',  '0',  '0',  '0',  '0');";
     if(mysql_query($sql_inserir))
-    {
-        return TRUE;
-    }
+      return TRUE;
     else
-    {
-        return FALSE;
-    }
+      return FALSE;
+     //return mysql_query($sql_update)? TRUE:FALSE;
 }
 
-function _update($Email,$Nome,$Senha,$Telefone1,$Telefone2,$Data_Nascimento,$Foto,$Sexo)
+function update($nome,$email,$sexo,$telefone,$celular,$senha,$foto,$status)
 {
     $sql_update = "	UPDATE  `u961758316_uefs`.`usuarios`
     SET  
-    `Email` =  '$Email',
-    `name` = '$Nome',
-    `Senha` = '$Senha',
-    `Telefone1` = '$Telefone1',
-    `Telefone2` = '$Telefone2',
-    `Sexo` = '$Sexo',
-    `Foto` = '$Foto',
-    `Data_Nascimento` = '$Data_Nascimento'
-    WHERE `usuarios`.`Email` =  '$Email';";
+    `Email` =  '$email',
+    `name` = '$nome',
+    `Senha` = '$senha',
+    `Telefone1` = '$telefone',
+    `Telefone2` = '$celular',
+    `Sexo` = '$sexo',
+    `Foto` = '$foto',
+    `Data_Nascimento` = NULL;
+    WHERE `usuarios`.`Email` =  '$email';";
 
-    if(mysql_query($sql_update))
-    {
-        return TRUE;
-    }
-    else
-    {
-        return FALSE;
-    }
+    return mysql_query($sql_update)? TRUE:FALSE;
 }
-function _findUser($Email)//recover
+function buscarUsuario($email)//recover
 {
     $sql = "SELECT * FROM `u961758316_uefs`.`usuarios`;";
     $sql_sel = mysql_query($sql);
 
     while($ln = mysql_fetch_array($sql_sel))
-        if($ln['id']==$Email)
+        if($ln['id']==$email)
             return $ln;
 }
 /*
@@ -65,14 +54,11 @@ function _findUser($Email)//recover
         Mas nao iremos deletar USUARIOS...
         mas servirá para anuncios.
 */
-function _delete($Email)
+function deleta($email)
 {
-    $sql = " DELETE FROM `u961758316_uefs`.`usuarios` WHERE Email = '$Email';";
+    $sql = " DELETE FROM `u961758316_uefs`.`usuarios` WHERE Email = '$email';";
 
-        if(mysql_query($sql))
-            return TRUE;
-        else
-            return FALSE;
+     return mysql_query($sql)? TRUE:FALSE;
 }
 ?>
 
