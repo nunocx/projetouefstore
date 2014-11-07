cfc21538608bcb11741d52eeed529d529f7be16e
arthurferroni <arthur.ferroni@live.com>
2014-11-07 09:19:31 -0300

    Admim marcelo

--- UesfStore/Src/DAO/MySqlClass.php
+++ UesfStore/Src/DAO/MySqlClass.php
@@ -3,17 +3,17 @@
 
 function conexao()
 {   
-    /*
+    
     $bd  ='u961758316_uefs';
     $user='root';
     $pass='';
     $host='localhost';
-    */
+    /*
     $bd  ='u961758316_uefs';
     $host='mysql.hostinger.com.br';
     $user='u961758316_admin';
     $pass='123456789';
-
+    */
     $conn = @mysql_connect($host,$user,$pass) or die ('Error Na rotina de conexao: '.mysql_error());
     mysql_select_db($bd) or die ('Error ao selecionar banco de dados: '.mysql_error());
     //resolvendo problemas de acentuações
@@ -21,44 +21,6 @@ function conexao()
     mysql_query("SET character_set_connection=utf8");
     mysql_query("SET character_set_client=utf8");
     mysql_query("SET character_set_results=utf8");
-}
+}	
 
-function inserir($sql)
-{
-    if(mysql_query($sql))
-    {
-        return TRUE;
-    }
-    else
-    {
-        return FALSE;
-    }
-}
-function seleciona($sql)
-{
-    return mysql_query($sql);
-}
-
-function deletar($sql)
-{
-    if(mysql_query($sql))
-    {
-        return TRUE;
-    }
-    else
-    {
-        return FALSE;
-    }	
-}
-function update($sql)
-{
-    if(mysql_query($sql))
-    {
-        return TRUE;
-    }
-    else
-    {
-        return FALSE;
-    }	
-}
 ?>
