78ebf040a757ecd58168b96d4a6d1febace7dd46
nielvag <nielsonvagno@gmail.com>
2014-11-29 22:36:54 -0300

    Relatórios (imcompletos)
    a parte de negociações está completa, pelo menos os gráficos com
    estátisticas mais principais. falta de anúncios e usuários ainda, vou
    terminar amanhã.
    qualquer dúvida, SUGESTÃO, to por aqui

--- PainelAdmim/app/Controller/GerenciamentosController.php
+++ PainelAdmim/app/Controller/GerenciamentosController.php
@@ -131,6 +131,243 @@ class GerenciamentosController extends AppController {
 	    //$graph->img->Stream();
 	}
 
+		public function negociacoesAno() {
+		App::import('Vendor', 'jpgraph/jpgraph');
+    	App::import('Vendor', 'jpgraph/jpgraph_line');
+
+		$data[0] = date('d/m/Y', strtotime("-11 month"));
+		$data[1] = date('d/m/Y', strtotime("-10 month"));
+		$data[2] = date('d/m/Y', strtotime("-9 month"));
+		$data[3] = date('d/m/Y', strtotime("-8 month"));
+		$data[4] = date('d/m/Y', strtotime("-7 month"));
+		$data[5] = date('d/m/Y', strtotime("-6 month"));
+		$data[6] = date('d/m/Y', strtotime("-5 month"));
+		$data[7] = date('d/m/Y', strtotime("-4 month "));
+		$data[8] = date('d/m/Y', strtotime("-3 month "));
+		$data[9] = date('d/m/Y', strtotime("-2 month "));
+		$data[10] = date('d/m/Y', strtotime("-1 month "));
+		$data[11] = date('d/m/Y');
+		//debug($data) or die();
+
+		$data2[0] = implode("-",array_reverse(explode("/",$data[0])));
+		$data2[1] = implode("-",array_reverse(explode("/",$data[1])));
+		$data2[2] = implode("-",array_reverse(explode("/",$data[2])));
+		$data2[3] = implode("-",array_reverse(explode("/",$data[3])));
+		$data2[4] = implode("-",array_reverse(explode("/",$data[4])));
+		$data2[5] = implode("-",array_reverse(explode("/",$data[5])));
+		$data2[6] = implode("-",array_reverse(explode("/",$data[6])));
+		$data2[7] = implode("-",array_reverse(explode("/",$data[7])));
+		$data2[8] = implode("-",array_reverse(explode("/",$data[8])));
+		$data2[9] = implode("-",array_reverse(explode("/",$data[9])));
+		$data2[10] = implode("-",array_reverse(explode("/",$data[10])));
+		$data2[11] = implode("-",array_reverse(explode("/",$data[11])));
+
+	    $qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` >= '".$data2[0]."' AND `negociacaos`.`data_final` < '".$data2[1]."' AND `negociacaos`.`status` = 1;";
+	    $valores[0] = $this->requestAction('/negociacaos/search/'.$qtd);
+	    $qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` >= '".$data2[1]."' AND `negociacaos`.`data_final` < '".$data2[2]."' AND `negociacaos`.`status` = 1;";
+	    $valores[1] = $this->requestAction('/negociacaos/search/'.$qtd);
+	    $qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` >= '".$data2[2]."' AND `negociacaos`.`data_final` < '".$data2[3]."' AND `negociacaos`.`status` = 1;";
+	    $valores[2] = $this->requestAction('/negociacaos/search/'.$qtd);
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` >= '".$data2[3]."' AND `negociacaos`.`data_final` < '".$data2[4]."' AND `negociacaos`.`status` = 1;";
+		$valores[3] = $this->requestAction('/negociacaos/search/'.$qtd);
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` >= '".$data2[4]."' AND `negociacaos`.`data_final` < '".$data2[5]."' AND `negociacaos`.`status` = 1;";
+		$valores[4] = $this->requestAction('/negociacaos/search/'.$qtd);
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` >= '".$data2[5]."' AND `negociacaos`.`data_final` < '".$data2[6]."' AND `negociacaos`.`status` = 1;";
+		$valores[5] = $this->requestAction('/negociacaos/search/'.$qtd);
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` >= '".$data2[6]."' AND `negociacaos`.`data_final` < '".$data2[7]."' AND `negociacaos`.`status` = 1;";
+		$valores[6] = $this->requestAction('/negociacaos/search/'.$qtd);
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` >= '".$data2[7]."' AND `negociacaos`.`data_final` < '".$data2[8]."' AND `negociacaos`.`status` = 1;";
+		$valores[7] = $this->requestAction('/negociacaos/search/'.$qtd);
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` >= '".$data2[8]."' AND `negociacaos`.`data_final` < '".$data2[9]."' AND `negociacaos`.`status` = 1;";
+		$valores[8] = $this->requestAction('/negociacaos/search/'.$qtd);
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` >= '".$data2[9]."' AND `negociacaos`.`data_final` < '".$data2[10]."' AND `negociacaos`.`status` = 1;";
+		$valores[9] = $this->requestAction('/negociacaos/search/'.$qtd);
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` >= '".$data2[10]."' AND `negociacaos`.`data_final` < '".$data2[11]."' AND `negociacaos`.`status` = 1;";
+		$valores[10] = $this->requestAction('/negociacaos/search/'.$qtd);
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` = '".$data2[11]."' AND `negociacaos`.`status` = 1;";
+		$valores[11] = $this->requestAction('/negociacaos/search/'.$qtd);
+
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` >= '".$data2[0]."' AND `negociacaos`.`data_final` < '".$data2[1]."' AND `negociacaos`.`status` = 0;";
+	    $valores2[0] = $this->requestAction('/negociacaos/search/'.$qtd);
+	    $qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` >= '".$data2[1]."' AND `negociacaos`.`data_final` < '".$data2[2]."' AND `negociacaos`.`status` = 0;";
+	    $valores2[1] = $this->requestAction('/negociacaos/search/'.$qtd);
+	    $qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` >= '".$data2[2]."' AND `negociacaos`.`data_final` < '".$data2[3]."' AND `negociacaos`.`status` = 0;";
+	    $valores2[2] = $this->requestAction('/negociacaos/search/'.$qtd);
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` >= '".$data2[3]."' AND `negociacaos`.`data_final` < '".$data2[4]."' AND `negociacaos`.`status` = 0;";
+		$valores2[3] = $this->requestAction('/negociacaos/search/'.$qtd);
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` >= '".$data2[4]."' AND `negociacaos`.`data_final` < '".$data2[5]."' AND `negociacaos`.`status` = 0;";
+		$valores2[4] = $this->requestAction('/negociacaos/search/'.$qtd);
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` >= '".$data2[5]."' AND `negociacaos`.`data_final` < '".$data2[6]."' AND `negociacaos`.`status` = 0;";
+		$valores2[5] = $this->requestAction('/negociacaos/search/'.$qtd);
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` >= '".$data2[6]."' AND `negociacaos`.`data_final` < '".$data2[7]."' AND `negociacaos`.`status` = 0;";
+		$valores2[6] = $this->requestAction('/negociacaos/search/'.$qtd);
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` >= '".$data2[7]."' AND `negociacaos`.`data_final` < '".$data2[8]."' AND `negociacaos`.`status` = 0;";
+		$valores2[7] = $this->requestAction('/negociacaos/search/'.$qtd);
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` >= '".$data2[8]."' AND `negociacaos`.`data_final` < '".$data2[9]."' AND `negociacaos`.`status` = 0;";
+		$valores2[8] = $this->requestAction('/negociacaos/search/'.$qtd);
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` >= '".$data2[9]."' AND `negociacaos`.`data_final` < '".$data2[10]."' AND `negociacaos`.`status` = 0;";
+		$valores2[9] = $this->requestAction('/negociacaos/search/'.$qtd);
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` >= '".$data2[10]."' AND `negociacaos`.`data_final` < '".$data2[11]."' AND `negociacaos`.`status` = 0;";
+		$valores2[10] = $this->requestAction('/negociacaos/search/'.$qtd);
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` = '".$data2[11]."' AND `negociacaos`.`status` = 0;";
+		$valores2[11] = $this->requestAction('/negociacaos/search/'.$qtd);
+		
+	    $ydata = array($valores[0][0][0]['COUNT(*)'], $valores[1][0][0]['COUNT(*)'] , $valores[2][0][0]['COUNT(*)'] ,
+	    				 $valores[3][0][0]['COUNT(*)'] , $valores[4][0][0]['COUNT(*)'] , $valores[5][0][0]['COUNT(*)']
+	    				 , $valores[6][0][0]['COUNT(*)'], $valores[7][0][0]['COUNT(*)'], $valores[8][0][0]['COUNT(*)']
+	    				 , $valores[9][0][0]['COUNT(*)'], $valores[10][0][0]['COUNT(*)'], $valores[11][0][0]['COUNT(*)']);
+	    
+	    $ydatas = array($valores2[0][0][0]['COUNT(*)'], $valores2[1][0][0]['COUNT(*)'] , $valores2[2][0][0]['COUNT(*)'] ,
+	    				 $valores2[3][0][0]['COUNT(*)'] , $valores2[4][0][0]['COUNT(*)'] , $valores2[5][0][0]['COUNT(*)']
+	    				  , $valores2[6][0][0]['COUNT(*)'] , $valores2[7][0][0]['COUNT(*)'] , $valores2[8][0][0]['COUNT(*)']
+	    				   , $valores2[9][0][0]['COUNT(*)'] , $valores2[10][0][0]['COUNT(*)'] , $valores2[11][0][0]['COUNT(*)']);
+	    // Create the graph. These two calls are always required
+	    $graph = new Graph(655,450);
+	    $graph->SetScale('textlin');
+
+	    for($i = 0; $i < 12; $i++) {
+	    	if(substr($data[$i], 3, 2) == "01") { $data[$i] = "Jan"; }
+	    	if(substr($data[$i], 3, 2) == "02") { $data[$i] = "Fev"; }
+	    	if(substr($data[$i], 3, 2) == "03") { $data[$i] = "Mar"; }
+	    	if(substr($data[$i], 3, 2) == "04") { $data[$i] = "Abr"; }
+	    	if(substr($data[$i], 3, 2) == "05") { $data[$i] = "Mai"; }
+	    	if(substr($data[$i], 3, 2) == "06") { $data[$i] = "jun"; }
+	    	if(substr($data[$i], 3, 2) == "07") { $data[$i] = "Jul"; }
+	    	if(substr($data[$i], 3, 2) == "08") { $data[$i] = "Ago"; }
+	    	if(substr($data[$i], 3, 2) == "09") { $data[$i] = "Set"; }
+	    	if(substr($data[$i], 3, 2) == "10") { $data[$i] = "Out"; }
+	    	if(substr($data[$i], 3, 2) == "11") { $data[$i] = "Nov"; }
+	    	if(substr($data[$i], 3, 2) == "12") { $data[$i] = "Dez"; }
+	    }
+
+	    $graph->xaxis->SetTickLabels(array($data[0], $data[1], $data[2], $data[3], $data[4], $data[5], $data[6], $data[7], $data[8], $data[9], $data[10], "Atual"));
+	    // Create the linear plot
+	    $lineplot=new LinePlot($ydata);
+	    $theme_class=new UniversalTheme;
+		$graph->img->SetAntiAliasing(false);
+		$graph->SetBox(false);
+	    // Add the plot to the graph
+	    $graph->Add($lineplot);
+	    $lineplot->SetColor('yellow');
+	    $lineplot->SetLegend('Negociações Realizadas');
+
+	    $lineplot2=new LinePlot($ydatas);
+	    $graph->Add($lineplot2);
+	    $lineplot2->SetColor('green');
+	    $lineplot2->SetLegend('Negociações não Realizadas');
+	    $graph->title->Set("Negociações no Ano");
+
+	    // Get the handler to prevent the library from sending the
+	    // image to the browser
+	    $gdImgHandler = $graph->Stroke(_IMG_HANDLER);
+
+	    // Stroke image to a file
+
+	    // Default is PNG so use ".png" as suffix
+	    $fileName = "img/relatorios/negociacoesAno.png";
+	    $graph->img->Stream($fileName);
+
+	    // Send it back to browser
+	    //$graph->img->Headers();
+	    //$graph->img->Stream();
+	}
+
+	public function negociacoesSem() {
+		App::import('Vendor', 'jpgraph/jpgraph');
+    	App::import('Vendor', 'jpgraph/jpgraph_line');
+
+		$data[0] = date('d/m/Y', strtotime("-1 days"));
+		$data[1] = date('d/m/Y', strtotime("-5 days"));
+		$data[2] = date('d/m/Y', strtotime("-4 days"));
+		$data[3] = date('d/m/Y', strtotime("-3 days"));
+		$data[4] = date('d/m/Y', strtotime("-2 days"));
+		$data[5] = date('d/m/Y', strtotime("-1 days"));
+		$data[6] = date('d/m/Y');
+
+		$data2[0] = implode("-",array_reverse(explode("/",$data[0])));
+		$data2[1] = implode("-",array_reverse(explode("/",$data[1])));
+		$data2[2] = implode("-",array_reverse(explode("/",$data[2])));
+		$data2[3] = implode("-",array_reverse(explode("/",$data[3])));
+		$data2[4] = implode("-",array_reverse(explode("/",$data[4])));
+		$data2[5] = implode("-",array_reverse(explode("/",$data[5])));
+		$data2[6] = implode("-",array_reverse(explode("/",$data[6])));
+
+	    $qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` = '".$data2[0]."' AND `negociacaos`.`status` = 1;";
+	    $valores[0] = $this->requestAction('/negociacaos/search/'.$qtd);
+	    $qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` = '".$data2[1]."' AND `negociacaos`.`status` = 1;";
+	    $valores[1] = $this->requestAction('/negociacaos/search/'.$qtd);
+	    $qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` = '".$data2[2]."' AND `negociacaos`.`status` = 1;";
+	    $valores[2] = $this->requestAction('/negociacaos/search/'.$qtd);
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` = '".$data2[3]."' AND `negociacaos`.`status` = 1;";
+		$valores[3] = $this->requestAction('/negociacaos/search/'.$qtd);
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` = '".$data2[4]."' AND `negociacaos`.`status` = 1;";
+		$valores[4] = $this->requestAction('/negociacaos/search/'.$qtd);
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` = '".$data2[5]."' AND `negociacaos`.`status` = 1;";
+		$valores[5] = $this->requestAction('/negociacaos/search/'.$qtd);
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` = '".$data2[6]."' AND `negociacaos`.`status` = 1;";
+		$valores[6] = $this->requestAction('/negociacaos/search/'.$qtd);
+
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` = '".$data2[0]."' AND `negociacaos`.`status` = 0;";
+	    $valores2[0] = $this->requestAction('/negociacaos/search/'.$qtd);
+	    $qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` = '".$data2[1]."' AND `negociacaos`.`status` = 0";
+	    $valores2[1] = $this->requestAction('/negociacaos/search/'.$qtd);
+	    $qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` = '".$data2[2]."' AND `negociacaos`.`status` = 0;";
+	    $valores2[2] = $this->requestAction('/negociacaos/search/'.$qtd);
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` = '".$data2[3]."' AND `negociacaos`.`status` = 0;";
+		$valores2[3] = $this->requestAction('/negociacaos/search/'.$qtd);
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` = '".$data2[4]."' AND `negociacaos`.`status` = 0;";
+		$valores2[4] = $this->requestAction('/negociacaos/search/'.$qtd);
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` = '".$data2[5]."' AND `negociacaos`.`status` = 0;";
+		$valores2[5] = $this->requestAction('/negociacaos/search/'.$qtd);
+		$qtd = "SELECT COUNT(*) FROM `negociacaos` WHERE `negociacaos`.`data_final` = '".$data2[6]."' AND `negociacaos`.`status` = 0;";
+		$valores2[6] = $this->requestAction('/negociacaos/search/'.$qtd);
+		
+	    $ydata = array($valores[0][0][0]['COUNT(*)'], $valores[1][0][0]['COUNT(*)'] , $valores[2][0][0]['COUNT(*)'] ,
+	    				 $valores[3][0][0]['COUNT(*)'] , $valores[4][0][0]['COUNT(*)'] , $valores[5][0][0]['COUNT(*)'] , $valores[6][0][0]['COUNT(*)']);
+	    
+	    $ydatas = array($valores2[0][0][0]['COUNT(*)'], $valores2[1][0][0]['COUNT(*)'] , $valores2[2][0][0]['COUNT(*)'] ,
+	    				 $valores2[3][0][0]['COUNT(*)'] , $valores2[4][0][0]['COUNT(*)'] , $valores2[5][0][0]['COUNT(*)'], $valores2[6][0][0]['COUNT(*)']);
+	    //debug($valores[0][0][0]['COUNT(*)']) or die();
+	    $graph = new Graph(655,450);
+	    $graph->SetScale('textlin');
+
+	    $graph->xaxis->SetTickLabels(array($data[0], $data[1], $data[2], $data[3], $data[4], $data[5], $data[6]));
+
+	    // Create the linear plot
+	    $lineplot=new LinePlot($ydata);
+	    $theme_class=new UniversalTheme;
+		$graph->img->SetAntiAliasing(false);
+		$graph->SetBox(false);
+	    // Add the plot to the graph
+	    $graph->Add($lineplot);
+	    $lineplot->SetColor('yellow');
+	    $lineplot->SetLegend('Negociações Realizadas');
+
+	    $lineplot2=new LinePlot($ydatas);
+	    $graph->Add($lineplot2);
+	    $lineplot2->SetColor('green');
+	    $lineplot2->SetLegend('Negociações não Realizadas');
+	    $graph->title->Set("Negociações na Semana");
+
+	    // Get the handler to prevent the library from sending the
+	    // image to the browser
+	    $gdImgHandler = $graph->Stroke(_IMG_HANDLER);
+
+	    // Stroke image to a file
+
+	    // Default is PNG so use ".png" as suffix
+	    $fileName = "img/relatorios/negociacoesSem.png";
+	    $graph->img->Stream($fileName);
+
+	    // Send it back to browser
+	    //$graph->img->Headers();
+	    //$graph->img->Stream();
+	}
+
+	public function negociacoes(){
+
+	}
+
 	public function anuncios(){
 		App::import('Vendor', 'jpgraph/jpgraph');
     	App::import('Vendor', 'jpgraph/jpgraph_bar');
