1d45e49ebc16cd49394738647179419501449f63
higorvital <h.vitall96@gmail.com>
2014-12-02 13:31:15 -0200

    Create connect_i.php

---
+++ UEFStoreCakePhp/app/View/Mensagems/includes/connect_i.php
@@ -0,0 +1,9 @@
+<?php
+
+$db_host = "localhost";
+$db_username = "root";
+$db_pass = "";
+$db_name ="u961758316_uefs";
+
+$myConnection = mysqli_connect("$db_host","$db_username","$db_pass","$db_name") or die ("Could Not connect to mysql");
+?>
