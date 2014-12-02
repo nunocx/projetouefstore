235f9528a98f20ea3f1b941bb0b04ae41a224ca9
higorvital <h.vitall96@gmail.com>
2014-12-02 13:36:51 -0200

    Update connect_i.php

--- UEFStoreCakePhp/app/View/Mensagems/includes/connect_i.php
+++ UEFStoreCakePhp/app/View/Mensagems/includes/connect_i.php
@@ -5,5 +5,12 @@ $db_username = "root";
 $db_pass = "";
 $db_name ="u961758316_uefs";
 
+/* Configuração Online
+$host='mysql.hostinger.com.br';
+$user='u961758316_admin';
+$pass='123456789';
+$db  ='u961758316_uefs';
+*/
+
 $myConnection = mysqli_connect("$db_host","$db_username","$db_pass","$db_name") or die ("Could Not connect to mysql");
 ?>
