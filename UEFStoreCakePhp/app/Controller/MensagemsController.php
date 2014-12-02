a7564049c2750cd7e64f85682c293703f16cf7de
arthurferroni <arthur.ferroni@live.com>
2014-12-02 11:18:24 -0200

    Mensagem Concertadas
    IGOR APRENDE UM COISA NA VIDA, quando vc muda algo que já está
    implementado, vc tem que testas outras coisas tbm, vc modificou uma
    cosia e danificou outra !!! affs, nao vou ficar concertando erro toda
    hora nao chega disso..
    nao é apenas substituir vc tem que olha o que a class tem diferente da
    sua e colocar as coisas que vc fez de diferente man.. serio .

--- UEFStoreCakePhp/app/Controller/MensagemsController.php
+++ UEFStoreCakePhp/app/Controller/MensagemsController.php
@@ -25,22 +25,13 @@ public function beforeFilter()
         parent::beforeFilter();
          $this->Auth->allowedActions = array('add');                        
     }
-		public function index() {
-			
-			$mensagems = $this->paginate();
-	        if ($this->request->is('requested')) {   //Se for requisição de outra view/element:
-	            return $mensagems;
-	        } else {  //Senão envia para a view padrão
-	            $this->set('mensagems', $mensagems);
-	        }
-		}
-
+	
 /**
  * index method
  *
  * @return void
  */
-		public function index() {
+		public function index(){
 			
 			$mensagems = $this->paginate();
 	        if ($this->request->is('requested')) {   //Se for requisição de outra view/element:
@@ -131,6 +122,19 @@ public function beforeFilter()
 			
 			
 		}
+		public function add() {
+		if ($this->request->is('post')) {
+			$this->Mensagem->create();
+
+			$this->request->data['Mensagem']['Data'] = date('Y-m-d');
+			if ($this->Mensagem->save($this->request->data)) {
+				$this->Session->setFlash(__('Obrigado sua mensagem foi enviada com sucesso.'));
+				return $this->redirect('/');
+			} else {
+				$this->Session->setFlash(__('Occoreu um erro ao envio de sua mensagem.'));
+			}
+		}
+	}
 
 
 }
