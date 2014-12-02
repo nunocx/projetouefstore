bc1ad06c8174b5260975680dc69e74b68915a6f1
higorvital <h.vitall96@gmail.com>
2014-12-02 04:06:03 -0200

    Update MensagemsController.php

--- UEFStoreCakePhp/app/Controller/MensagemsController.php
+++ UEFStoreCakePhp/app/Controller/MensagemsController.php
@@ -36,80 +36,101 @@ public function beforeFilter()
 		}
 
 /**
- * view method
+ * index method
  *
- * @throws NotFoundException
- * @param string $id
  * @return void
  */
-	public function view($id = null) {
-		if (!$this->Mensagem->exists($id)) {
-			throw new NotFoundException(__('Invalid mensagem'));
+		public function index() {
+			
+			$mensagems = $this->paginate();
+	        if ($this->request->is('requested')) {   //Se for requisição de outra view/element:
+	            return $mensagems;
+	        } else {  //Senão envia para a view padrão
+	            $this->set('mensagems', $mensagems);
+	        }
 		}
-		$options = array('conditions' => array('Mensagem.' . $this->Mensagem->primaryKey => $id));
-		$this->set('mensagem', $this->Mensagem->find('first', $options));
-	}
+		public function pm_inbox() {
+	
+		}
+		public function pm_outbox() {
+		
+	
+		}
+		public function pm_send() {
+			
+			
+		}
+		public function pm_send_to($id = NULL,$id1 = NULL) {
+				
+				$i = $id;
+				$ip = $id1;
 
-/**
- * add method
- *
- * @return void
- */
-	public function add() {
-		if ($this->request->is('post')) {
-			$this->Mensagem->create();
+				if($ip>0){
 
-			$this->request->data['Mensagem']['Data'] = date('Y-m-d');
-			if ($this->Mensagem->save($this->request->data)) {
-				$this->Session->setFlash(__('Obrigado sua mensagem foi enviada com sucesso.'));
-				return $this->redirect('/');
-			} else {
-				$this->Session->setFlash(__('Occoreu um erro ao envio de sua mensagem.'));
+					$sql = "SELECT * FROM `u961758316_uefs`.`usuarios` WHERE `usuarios`.`id` = $i"; 
+			$usuario = $this->Mensagem->query($sql);
+
+			if($usuario ==null)
+			{
+				throw new NotFoundException("Usuário não encontrado!");
 			}
-		}
-	}
-/**
- * edit method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function edit($id = null) {
-		if (!$this->Mensagem->exists($id)) {
-			throw new NotFoundException(__('Invalid mensagem'));
-		}
-		if ($this->request->is(array('post', 'put'))) {
-			if ($this->Mensagem->save($this->request->data)) {
-				$this->Session->setFlash(__('The mensagem has been saved.'));
-				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The mensagem could not be saved. Please, try again.'));
-			}
-		} else {
-			$options = array('conditions' => array('Mensagem.' . $this->Mensagem->primaryKey => $id));
-			$this->request->data = $this->Mensagem->find('first', $options);
-		}
-	}
 
-/**
- * delete method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function delete($id = null) {
-		$this->Mensagem->id = $id;
-		if (!$this->Mensagem->exists()) {
-			throw new NotFoundException(__('Invalid mensagem'));
+			foreach ($usuario as $usuarios) {
+				$usuario = $usuarios;
+			}
+			//debug($usuario['usuarios']['name']) or die ();
+			$this->set('idUser', $usuario['usuarios']['id']);
+			
+			$sql = "SELECT * FROM `u961758316_uefs`.`produtos` WHERE `produtos`.`id` = $ip"; 
+			$produto = $this->Mensagem->query($sql);
+
+			if($produto ==null)
+			{
+				throw new NotFoundException("Produto não encontrado!");
+			}
+
+			foreach ($produto as $produtos) {
+				$produto = $produtos;
+			}
+			//debug($usuario['usuarios']['name']) or die ();
+			$this->set('idProduct', $produtos['produtos']['id']);
+
+			}
+
+			else{
+
+						$sql = "SELECT * FROM `u961758316_uefs`.`usuarios` WHERE `usuarios`.`id` = $i"; 
+			$usuario = $this->Mensagem->query($sql);
+
+			if($usuario ==null)
+			{
+				throw new NotFoundException("Usuário não encontrado!");
+			}
+
+			foreach ($usuario as $usuarios) {
+				$usuario = $usuarios;
+			}
+			//debug($usuario['usuarios']['name']) or die ();
+			$this->set('idUser', $usuario['usuarios']['id']);
+
+			$this->set('idProduct', "");
+
+			}
+
+	
 		}
-		$this->request->allowMethod('post', 'delete');
-		if ($this->Mensagem->delete()) {
-			$this->Session->setFlash(__('The mensagem has been deleted.'));
-		} else {
-			$this->Session->setFlash(__('The mensagem could not be deleted. Please, try again.'));
+
+		public function message(){
+			
 		}
-	return $this->redirect('/');
-	}
+		public function pm_view_out() {
+			
+			
+		}
+		public function pm_view_in() {
+			
+			
+		}
+
+
 }
