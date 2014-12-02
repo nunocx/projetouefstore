6ff6b209b53bea7c01cfb1d47ded50f42b6edff9
higorvital <h.vitall96@gmail.com>
2014-12-02 13:36:22 -0200

    Update connect.php

--- UEFStoreCakePhp/app/View/Mensagems/includes/connect.php
+++ UEFStoreCakePhp/app/View/Mensagems/includes/connect.php
@@ -5,6 +5,13 @@ $user = "root";
 $pass = "";
 $db = "u961758316_uefs";
 
+/* Configuração Online
+$host='mysql.hostinger.com.br';
+$user='u961758316_admin';
+$pass='123456789';
+$db  ='u961758316_uefs';
+*/
+
 mysql_connect($host,$user,$pass);
 mysql_select_db($db);
 
